6ec0998c998364f87b292e2937ab573379b2da1b
icemabel <hande_barkan@hotmail.com>
2025-01-22 09:34:57 +0100

    fix: BootstrapDataTest

--- src/test/java/com/hande/RestfulDemo/bootstrap/BootstrapDataTest.java
+++ src/test/java/com/hande/RestfulDemo/bootstrap/BootstrapDataTest.java
@@ -3,15 +3,18 @@ package com.hande.RestfulDemo.bootstrap;
 import com.hande.RestfulDemo.repositories.BeerRepository;
 import com.hande.RestfulDemo.repositories.CustomerRepository;
 import com.hande.RestfulDemo.services.BeerCsvService;
+import com.hande.RestfulDemo.services.BeerCsvServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@Import(BeerCsvServiceImpl.class)
 class BootstrapDataTest {
     @Autowired
     BeerRepository beerRepository;
@@ -19,9 +22,11 @@ class BootstrapDataTest {
     @Autowired
     CustomerRepository customerRepository;
 
-    BootstrapData bootstrapData;
     @Autowired
     private BeerCsvService beerCsvService;
+
+    BootstrapData bootstrapData;
+
 
     @BeforeEach
     void setUp() {
@@ -33,7 +38,7 @@ class BootstrapDataTest {
     void testRun() throws Exception {
         bootstrapData.run(null);
 
-        assertThat(beerRepository.count()).isEqualTo(3);
+        assertThat(beerRepository.count()).isEqualTo(2413);
         assertThat(customerRepository.count()).isEqualTo(3);
     }
 }
